91be395736be7f2f66d0bb7ca8b483e56005873a
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-27 15:27:57 +0530

    First Commit on socions

---
+++ esbuild.js
@@ -0,0 +1,16 @@
+const esbuild=require("esbuild")
+const defaultConfig={
+    entryPoints:["./src/index.js"],
+    bundle:true 
+}
+esbuild.build({
+    ...defaultConfig,
+    format:"esm",
+    outfile:"./dist/index.mjs"
+})
+
+esbuild.build({
+    ...defaultConfig,
+    format:"cjs",
+    outfile:"./dist/index.js"
+})
